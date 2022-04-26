25d488ac6f4e3dbd698ea3de0cd238bdfe3dcb28
gary <gary@MACSTATION.local>
2022-04-26 08:31:40 +0100

    Updates

--- test/IntegrationTests.cs
+++ test/IntegrationTests.cs
@@ -4,28 +4,44 @@ namespace SampleApi.Test.TokenIssuer
     using NUnit.Framework;
     using SampleApi.Test.Utils;
 
+    /*
+     * Test the API in isolation, without any dependencies on the Authorization Server
+     */
     [TestFixture]
     public class IntegrationTests
     {
+        // The real subject claim values for my two online test users
         private string guestUserId  = "a6b404b1-98af-41a2-8e7f-e4061dc0bf86";
         private string guestAdminId = "77a97e5b-b748-45e5-bb6f-658e85b2df91";
 
+        // A class to issue our own JWTs for testing
+        private TokenIssuer tokenIssuer;
 
+        /*
+         * Initialize mock token issuing and wiremock
+         */
         [OneTimeSetUp]
-        public async Task Setup()
+        public void Setup()
         {
-            var tokenIssuer = new TokenIssuer();
-            var keyset = tokenIssuer.GetTokenSigningPublicKeys();
+            this.tokenIssuer = new TokenIssuer();
+            var keyset = this.tokenIssuer.GetTokenSigningPublicKeys();
             TestContext.Progress.WriteLine(keyset);
-
-            var accessToken = tokenIssuer.IssueAccessToken(guestUserId);
-            TestContext.Progress.WriteLine(accessToken);
+        }
+        
+        /*
+         * Clean up resources after all tests have completed
+         */
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            this.tokenIssuer.Dispose();
         }
 
         [Test]
         public void GetUserClaims_ReturnsSingleRegion_ForStandardUser()
         {
-            TestContext.Progress.WriteLine("Test 1");
+            var accessToken = tokenIssuer.IssueAccessToken(guestUserId);
+            TestContext.Progress.WriteLine(accessToken);
             Assert.Pass();
         }
     }
--- test/utils/TokenIssuer.cs
+++ test/utils/TokenIssuer.cs
@@ -5,79 +5,101 @@ namespace SampleApi.Test.Utils
     using System.Security.Cryptography;
     using Jose;
 
-    public class TokenIssuer
+    /*
+     * A token issuer for testing
+     */
+    public class TokenIssuer : IDisposable
     {
         private readonly string algorithm;
+        private readonly RSA rsa;
         private readonly Jwk tokenSigningPrivateKey;
         private readonly Jwk tokenSigningPublicKey;
         private readonly string keyId;
 
+        /*
+         * Create keys for testing
+         */
         public TokenIssuer()
         {
             this.algorithm = "RS256";
             this.keyId = Guid.NewGuid().ToString();
 
-            using (var rsa = RSA.Create(2048))
-            {
-                this.tokenSigningPrivateKey = new Jwk(rsa.ExportRSAPrivateKey());
-                this.tokenSigningPrivateKey.Alg = this.algorithm;
-                //this.tokenSigningPrivateKey.Kty = "RSA";
-                this.tokenSigningPrivateKey.KeyId = this.keyId;
-                
-                this.tokenSigningPublicKey = new Jwk(rsa.ExportRSAPublicKey());
-                this.tokenSigningPublicKey.Alg = this.algorithm;
-                //this.tokenSigningPublicKey.Kty = "RSA";
-                this.tokenSigningPublicKey.KeyId = this.keyId;
-            }
+            this.rsa = RSA.Create(2048);
+
+            this.tokenSigningPrivateKey = new Jwk(rsa, true);
+            this.tokenSigningPrivateKey.Alg = this.algorithm;
+            this.tokenSigningPrivateKey.KeyId = this.keyId;
+            
+            this.tokenSigningPublicKey = new Jwk(rsa, false);
+            this.tokenSigningPublicKey.Alg = this.algorithm;
+            this.tokenSigningPublicKey.KeyId = this.keyId;
         }
 
+        /*
+         * Get the token signing public keys as a JSON Web Keyset
+         */
         public string GetTokenSigningPublicKeys()
         {
             var keyset = new JwkSet(this.tokenSigningPublicKey);
             return keyset.ToJson(JWT.DefaultSettings.JsonMapper);
         }
 
+        /*
+         * Issue an access token with the supplied subject claim
+         */
         public string IssueAccessToken(string subject)
         {
+            var now = DateTimeOffset.Now;
+            var iat = now.AddMinutes(-30);
+            var exp = now.AddMinutes(30);
+
             var payload = new Dictionary<string, object>()
             {
                 { "sub", subject },
                 { "iss", "testissuer.com" },
                 { "aud", "api.mycompany.com" },
-                { "exp", "whatevar" }, // FIX
+                { "iat", iat.ToUnixTimeSeconds() },
+                { "exp", exp.ToUnixTimeSeconds() },
                 { "scope", "openid profile email https://api.authsamples.com/api/transactions_read" }
             };
 
             return Jose.JWT.Encode(payload, this.tokenSigningPrivateKey, JwsAlgorithm.RS256);
-
-            /*
-            .setProtectedHeader( { kid: this._keyId, alg: this._algorithm } )
-            .setIssuedAt(now - 30000)
-            .setExpirationTime(now + 30000)
-            */
         }
 
+        /*
+         * Issue an access token signed with an untrusted JWK
+         */
         public string IssueMaliciousAccessToken(string subject)
         {
-            using (var rsa = RSA.Create(2048))
+            using (var maliciousRSA = RSA.Create(2048))
             {
-                var maliciousPrivateKey = new Jwk(rsa.ExportRSAPrivateKey());
+                var maliciousPrivateKey = new Jwk(maliciousRSA, true);
+                maliciousPrivateKey.Alg = this.algorithm;
+                maliciousPrivateKey.KeyId = this.keyId;
+
+                var now = DateTimeOffset.Now;
+                var iat = now.AddMinutes(-30);
+                var exp = now.AddMinutes(30);
                 
-                var maliciousPublicKey = new Jwk(rsa.ExportRSAPublicKey());
-                maliciousPublicKey.Alg = this.algorithm;
-                maliciousPublicKey.KeyId = this.keyId;
-            
                 var payload = new Dictionary<string, object>()
                 {
                     { "sub", subject },
                     { "iss", "testissuer.com" },
                     { "aud", "api.mycompany.com" },
-                    { "exp", "whatevar" }, // FIX
+                    { "iat", iat.ToUnixTimeSeconds() },
+                    { "exp", exp.ToUnixTimeSeconds() },
                     { "scope", "openid profile email https://api.authsamples.com/api/transactions_read" }
                 };
 
-                return Jose.JWT.Encode(payload, this.tokenSigningPrivateKey, JwsAlgorithm.RS256);
+                return Jose.JWT.Encode(payload, maliciousPrivateKey, JwsAlgorithm.RS256);
             }
+        }
+
+        /*
+         * Dispose the internal key
+         */
+        public void Dispose() {
+            this.rsa.Dispose();
         }
     }
 }
