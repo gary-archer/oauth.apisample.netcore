32c0c5827410979a2afbcfea3c02f7e52dd0da19
gary <gary@MACSTATION.local>
2022-09-10 08:01:33 +0100

    Updates

--- deployment/shared/docker-init.sh
+++ deployment/shared/docker-init.sh
@@ -5,17 +5,15 @@
 ############################################################
 
 #
-# See if the extra trusted certificates file is non-empty
+# Point to the trusted certificate bundle injected into the container
 #
 TRUSTED_CA_CERTS='/usr/local/share/certificates/trusted.ca.crt'
-if [ -s "$TRUSTED_CA_CERTS" ]; then
 
-  #
-  # If so then configure operating system trust
-  #
-  update-ca-certificates
-  if [ $? -ne 0 ]; then
-    echo 'Problem encountered updating root certificates'
-    exit 1
-  fi
+#
+# Configure operating system trust
+#
+update-ca-certificates
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered updating root certificates'
+  exit 1
 fi
