0f66a0d210f912c9ad57607c39514d1a5756482c
gary <gary@MACSTATION.local>
2022-10-02 20:32:42 +0100

    Updates

--- start.sh
+++ start.sh
@@ -30,10 +30,9 @@ fi
 if [ ! -d '../oauth.logs' ]; then
   mkdir '../oauth.logs'
 fi
-if [ -d '../oauth.logs/api' ]; then
-  rm -rf '../oauth.logs/api'
+if [ ! -d '../oauth.logs/api' ]; then
+  mkdir '../oauth.logs/api'
 fi
-mkdir '../oauth.logs/api'
 
 #
 # Then start listening
