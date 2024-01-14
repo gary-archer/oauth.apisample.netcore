fc02098856474ac95229148bd04e4c3be790d94b
gary <gary.archer36@gmail.com>
2024-01-14 10:44:45 +0000

    Updates

--- src/plumbing/utilities/ResponseErrorWriter.cs
+++ src/plumbing/utilities/ResponseErrorWriter.cs
@@ -1,29 +1,21 @@
 namespace SampleApi.Plumbing.Utilities
 {
-    using System.Net;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
     using SampleApi.Plumbing.Errors;
 
     /*
-     * Return an error response from the API to the client
+     * Return secure errors to clients
      */
     public static class ResponseErrorWriter
     {
         /*
-         * Deliver a controlled 500 response to the caller
+         * This blog's examples use a JSON response to provide client friendly OAuth errors
+         * When required, such as to inform clients how to integrate, a www-authenticate header can be added here
+         * - https://datatracker.ietf.org/doc/html/rfc6750#section-3
          */
         public static async Task WriteErrorResponse(HttpResponse response, ClientError error)
         {
-            // Add the standards based header if required
-            if (error.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                var realm = "mycompany.com";
-                var wwwAuthenticateHeader = $"Bearer realm=\"{realm}\", error=\"{error.ErrorCode}\", error_description=\"{error.Message}\"";
-                response.Headers["www-authenticate"] = wwwAuthenticateHeader;
-            }
-
-            // Also add a more client friendly JSON response with the same fields
             response.ContentType = "application/json";
             response.StatusCode = (int)error.StatusCode;
             await response.WriteAsync(error.ToResponseFormat().ToString());
