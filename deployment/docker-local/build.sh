c7494038f870309810300af014a642dada5c905d
gary <gary.archer36@gmail.com>
2024-02-29 18:28:26 +0000

    Updates

--- deployment/docker-local/build.sh
+++ deployment/docker-local/build.sh
@@ -27,7 +27,7 @@ fi
 #
 # Build the docker image
 #
-docker build -f deployment/docker-local/Dockerfile -t finalnetcoreapi:latest .
+docker build -f deployment/shared/Dockerfile -t finalnetcoreapi:latest .
 if [ $? -ne 0 ]; then
   echo 'Problem encountered building the API docker image'
   exit
