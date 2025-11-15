9fe23d9fa0fe0ae3e358e338d050e4fad724f505
gary <garyarcher36@gmail.com>
2025-11-15 08:54:35 +0000

    Updates

--- integration_tests.sh
+++ integration_tests.sh
@@ -9,5 +9,6 @@ cd test
 
 #
 # Run tests with this category and with output verbosity that includes test names
+# https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-test?tabs=dotnet-test-with-vstest
 #
-dotnet test --filter Category="Integration" -l "console;verbosity=normal"
+dotnet test --filter Category="Integration" --logger "console;verbosity=normal" --tl:off
--- load_test.sh
+++ load_test.sh
@@ -9,5 +9,6 @@ cd test
 
 #
 # Run tests with this category and with output verbosity that includes test names
+# https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-test?tabs=dotnet-test-with-vstest
 #
-dotnet test --filter Category="Load" -l "console;verbosity=normal"
+dotnet test --filter Category="Load" --logger "console;verbosity=normal" --tl:off
